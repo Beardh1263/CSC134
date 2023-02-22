7a5d531baff2871c268bb20b32505c8b732aa39f
Beardh1263 <125904100+Beardh1263@users.noreply.github.com>
2023-02-22 11:04:48 -0500

    M4T1 part one

---
+++ M4T1_Beard/main.cpp
@@ -0,0 +1,25 @@
+//CSC_134
+//M4T1 - Loops
+//Hunter Beard
+//2/22/23
+//Purpose of program is to demonstrate various uses of while.
+
+#include <iostream>
+using namespace std;
+void say_hello(int count) {
+  //repeat count times
+  int i=0; // index
+  while (i < count) {
+    cout << "Hello #" << i+1 << endl;
+    i++; // count up
+  }
+  return; // for void functions, return is optional (implied)
+}
+
+int main() {
+  //call a function
+  int times = 0;
+  cout << "How many times should I say Hello? ";
+  cin >> times;
+  say_hello(times);
+}
