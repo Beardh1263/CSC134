de0a418242cfaa6d0bf42191a69e7a4ef18e3b3b
Beardh1263 <125904100+Beardh1263@users.noreply.github.com>
2023-02-27 11:12:44 -0500

    M4T2

---
+++ M4T2_Beard/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+//CSC 134
+//M4T2 - Menus
+//Hunter Beard
+//2/27/23
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    // Print three line menu
+    // press 1 to blah etc
+    // cin an integer
+
+    return 0;
+}
