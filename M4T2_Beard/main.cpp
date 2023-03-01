cea46193f61366b82564cb074c1f171f3224e0bd
Beardh1263 <125904100+Beardh1263@users.noreply.github.com>
2023-03-01 11:37:58 -0500

    dice roll 3/1

--- M4T2_Beard/main.cpp
+++ M4T2_Beard/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 //CSC 134
 //M4T2 - Menus
@@ -23,12 +25,26 @@ void option_one() {
 }
 
 void option_two() {
-    //sum up five numbers
+    //dice roller
+    int d6_1, d6_2, d6_3; //three dice (6sided)
+    int total; //3d6
+    int seed = time(0);
+    srand(seed);
+    //roll the dice
+    d6_1 = (rand() % 6) +1; // 0-5, then add one
+    d6_2 = (rand() % 6) +1;
+    d6_3 = (rand() % 6) +1;
+    total = d6_1 +d6_2 + d6_3;
+    cout << "Rolling 3d6: ";
+    cout << d6_1 << " + " << d6_2 << " + " << d6_3;
+    cout << " = " << total << endl;
 }
 
 void option_three() {
-    //TODO: what does this even do
+    //sum and average of dice
 }
+
+// There is no option 4, it's just quit
 //main menu
 int main()
 {
@@ -37,20 +53,29 @@ int main()
     //do something based on the choice
     cout << "Welcome to the menu" << endl;
     cout << "Press 1 to count" << endl;
-    cout << "Press 2 to sum up numbers" << endl;
-    cout << "Press 3 to TODO" << endl; // not implemented yet
+    cout << "Press 2 to roll dice" << endl;
+    cout << "Press 3 to roll a character" << endl; // not implemented yet
     cout << "Choice: ";
     //input validation
     int choice;
     cin >> choice;
-    // user must pick 1, 2, or 3
-    while (choice < 1 || choice > 3) {
-        cout << "Please choose 1, 2, or 3: ";
+    // user must pick 1, 2, 3, or 4
+    while (choice < 1 || choice > 4) {
+        cout << "Please choose 1, 2, or 3, or 4: ";
         cin >> choice;
     }
     //if we get here, we know choice is valid
     if (choice == 1) {
         option_one(); //function call
+    }
+    if (choice == 2) {
+        option_two(); // the dice roller
+    }
+    if (choice == 3) {
+        option_three();
+    }
+    if (choice == 4) {
+        cout << "Quitting time!" << endl;
     }
 
     cout << "Thanks for using the menu." << endl;
